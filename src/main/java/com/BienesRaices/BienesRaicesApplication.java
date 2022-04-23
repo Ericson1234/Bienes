3b835f2795f60e335e52cdda14eb1b0fb538cceb
Ericson1234 <ericsontq06@gmail.com>
2022-04-23 00:24:59 -0600

    Avance

---
+++ src/main/java/com/BienesRaices/BienesRaicesApplication.java
@@ -0,0 +1,13 @@
+package com.BienesRaices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BienesRaicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BienesRaicesApplication.class, args);
+	}
+
+}
